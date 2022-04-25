f16436da9e4e53444e4529513545f18b0bed073d
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-25 19:37:29 +0100

    overriding

--- inheritance.py
+++ inheritance.py
@@ -7,6 +7,9 @@ class Pet:
     def show(self):
         print(f"I am {self.name} and I am {self.age} years old.")
 
+    def speak(self):
+        print('I do not know what to say')
+
 # Spesific Class
 class Cat(Pet):
     def speak(Self):
@@ -22,3 +25,13 @@ c = Cat('Milly', 4)
 c.show() # inherited show method from Upper class which is Pet Output: I am Milly and I am 4 years old.
 d = Dog('Jill', 5)
 d.show() # inherited show method from Upper class which is Pet Output: I am Jill and I am 5 years old.
+
+
+# Speak
+c.speak() # Miyaw   # Cat class speak Method
+d.speak() # Bark    # Dog class speak Method
+p.speak() # I do not know what to say # Pet class speak method
+
+# Overriding is an object-oriented programming feature that enables a child class 
+# to provide different implementation for a method that is already defined and/or 
+# implemented in its parent class or one of its parent classes.
