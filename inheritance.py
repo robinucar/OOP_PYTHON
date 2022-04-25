037adf9847fa93e64ef44ea158be05d7b0248554
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-25 20:00:30 +0100

    using super class

--- inheritance.py
+++ inheritance.py
@@ -12,8 +12,14 @@ class Pet:
 
 # Spesific Class
 class Cat(Pet):
+    def __init__(self, name, age,color):
+        super().__init__(name, age) # inherit from upper class
+        self.color = color
     def speak(Self):
         print("Meow")
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old. My color is {self.color}.")
 
 class Dog(Pet):
     def speak(self):
@@ -24,8 +30,8 @@ class Fish(Pet):
 
 p = Pet('Tim', 19)
 p.show() # I am Tim and I am 19 years old.
-c = Cat('Milly', 4)
-c.show() # inherited show method from Upper class which is Pet Output: I am Milly and I am 4 years old.
+c = Cat('Milly', 4, 'Black')
+c.show() # Using Cat show method (overriding) Output: I am Milly and I am 4 years old. My color is Black.
 d = Dog('Jill', 5)
 d.show() # inherited show method from Upper class which is Pet Output: I am Jill and I am 5 years old.
 f = Fish('Gold', 3) 
