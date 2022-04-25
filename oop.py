99340a63b052f878526940ab397a59827d9e8830
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-25 15:46:34 +0100

    age and update age

--- oop.py
+++ oop.py
@@ -1,11 +1,27 @@
 class Dog:
+
+    def __init__(self, name, age): # initialize
+        self.name = name
+        self.age = age
     def bark(self):
         print('bark')
 
-    def intro(self, name):
-        return f'Hello, My name is {name}'
+    
 
-d = Dog()
-d.bark()
-print(d.intro('Boddy'))
-print(type(d))
+    def birtdayPassed(self):
+        self.age = self.age + 1
+
+    def intro(self):
+        return f'Hello, My name is {self.name}. And I am {self.age} years old.'
+
+# d = Dog('Boddy', 3)
+# s = Dog('Sakir', 2)
+# s.isBirtdayPassed()
+# d.bark()
+# print(d.intro())
+# print(s.intro())
+
+b = Dog('Boddy', 3)
+b.birtdayPassed() # age is 4
+b.birtdayPassed() # age is 5 
+print(b.intro()) # Hello, My name is Boddy. And I am 5 years old.
