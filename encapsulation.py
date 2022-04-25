431969f2e04fb6187a79d20e7022d8d1d4eeb1b1
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-25 22:35:01 +0100

    getter and setter methods

--- encapsulation.py
+++ encapsulation.py
@@ -5,6 +5,14 @@ class Person:
         self.__nationality = nationality
         self.__gender = gender
 
-
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, value):
+         self.__name = value
+    
 p = Person('Robin', 34, 'British', 'Male')
-print(p.__age) #Output: AttributeError: 'Person' object has no attribute '__age'  - double _ make variable private
+# print(p.__age) #Output: AttributeError: 'Person' object has no attribute '__age'  - double _ make variable private
+print(p.get_name())
+p.set_name('Melsa')
+print(p.get_name())
